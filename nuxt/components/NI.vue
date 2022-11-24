dfad69d23b75552ae8626bf65773a5d74f1f6cec
JebemtiMaiku <niki.iff@hotmail.com>
2022-11-24 17:12:51 +0100

    first things first

---
+++ nuxt/components/NI.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="NI-container">
+    <div>
+      <h6>niklaus iff</h6>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.NI-container {
+	position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  div {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    width: 40%;
+    transform: translate(-50%,-50%) scale(1,1);
+  }
+}
+</style>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index-container">
+    <NI />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+	data() {
+		return {};
+	},
+  computed: {},
+	methods: {},
+}
+</script>
+
+<style lang="scss">
+</style>
