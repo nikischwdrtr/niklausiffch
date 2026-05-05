09eb1c2980b16d58083f25d5cbb2e02512457be4
niklaus <40233850+nikischwdrtr@users.noreply.github.com>
2026-05-05 20:28:04 +0200

    Refactor text formatting in Statement.vue
    Reformat text for improved readability in Statement.vue.

--- nuxt/components/Statement.vue
+++ nuxt/components/Statement.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="statement">
     <p>
-      a reflection on the ambiguity and hybridity of growing up in an
-      agglomeration, themes of identity, classism, and the feeling of being
-      'in-between' naturally emerge — caught between city and countryside, poor
-      and rich, the working class and academics, apartment buildings and family
-      homes.
+      a reflection on the ambiguity and hybridity of growing up in an agglomeration, themes of identity, classism,
+and the feeling of being ‘in-between’ naturally emerge — caught between city and countryside, poor and rich,
+the working class and academics, apartment buildings and family homes.
     </p>
 
     <p>
-      these experiences and emotions, often rooted in autobiographical moments,
-      are collected and stored across different spaces — not fully synchronized
-      or comprehensible. through this decentralized process, thoughts are
-      combined and translated into material output where concepts and influences
-      are subtly woven into titles, descriptions, and deliberate nuances. the
-      work should not present a single truth or straightforward interpretation;
-      instead, it should provoke reflection and encourage personal experience.
+      these experiences and emotions, often rooted in autobiographical moments, are collected and stored across
+different spaces — not fully synchronized or comprehensible. through this decentralized process, thoughts
+are combined and translated into material output where concepts and influences are subtly woven into
+titles, descriptions, and deliberate nuances. the work should not present a single truth or straightforward
+interpretation; instead, it should provoke reflection and encourage personal experience.
     </p>
   </div>
 </template>
